f8d915bfba63b0c615fb58e28f6c1810605a209a
Karol Szafrański <kszafranski@eclipsesource.com>
2020-10-14 18:18:45 +0200

    wip

--- scripts/trigger-and-observe.sh
+++ scripts/trigger-and-observe.sh
@@ -1,21 +1,32 @@
 #!/usr/bin/env bash
 
 # set -x
+# printenv
 
-GITHUB_TOKEN="$(gopass show ks/github-token/karolszafranski@gmail.com)"
+GITHUB_TOKEN="$(security find-generic-password -a $USER -s ES_REVIEW_SQUIRREL_GITHUB_TOKEN -w)"
 
 # git config --global user.email "rjs@eclipsesource.com"
 # git config --global user.name "Robert Schmidt"
 
-REMOTE="tb"
-EXECUTION_ID=$(date +%y%m%d%H%M)
-BRANCH_NAME="prefix_${EXECUTION_ID}"
-
 REPO_OWNER="tabris"
-REPO_NAME="tabris-ios"
+REPO_NAME="tabris-ios-review"
 
-WORKFLOW_ID="535074"
+REMOTE="tb"
+REMOTE_URL="ssh://git@github.com/${REPO_OWNER}/${REPO_NAME}.git"
+
+EXECUTION_ID=$(date +%y%m%d%H%M)
+# BRANCH_NAME="prefix_${EXECUTION_ID}"
+
+# "refs/changes/68/20768/2" =>> "changes/68/20768/2"
+BRANCH_NAME="${GERRIT_REFSPEC#refs/}"
+
+WORKFLOW_ID="535074" # es/tabris-ios
+WORKFLOW_ID="3000569" # tb/tabris-ios
+
 HEAD_SHA="$(git rev-list HEAD | head -n 1)"
+
+echo "Add remote \"${REMOTE}\":\"${REMOTE_URL}\""
+git remote add "${REMOTE}" "${REMOTE_URL}"
 
 echo "Create branch \"${BRANCH_NAME}\""
 git checkout -b $BRANCH_NAME
@@ -41,7 +52,7 @@ function update_runs_json_file {
 
 update_runs_json_file
 
-RUN_ID=$(jq -r ".workflow_runs[] | select( .head_sha == \"${HEAD_SHA}\" ) | select( .workflow_id == ${WORKFLOW_ID} ) | .id" $RUNS_OUTPUT_JSON_FILE)
+RUN_ID=$(jq -r ".workflow_runs | map(select( .workflow_id == ${WORKFLOW_ID} )) | map(select( .head_sha == \"${HEAD_SHA}\" )) | sort_by( .run_number ) | .[-1] | .id" $RUNS_OUTPUT_JSON_FILE)
 
 echo "You can observe the logs at: https://github.com/${REPO_OWNER}/${REPO_NAME}/actions/runs/${RUN_ID}"
 
@@ -49,7 +60,7 @@ while sleep 5; do
 
 	update_runs_json_file
 
-	STATUS=$(jq -r ".workflow_runs[] | select( .head_sha == \"${HEAD_SHA}\" ) | select( .workflow_id == ${WORKFLOW_ID} ) | .status" $RUNS_OUTPUT_JSON_FILE)
+	STATUS=$(jq -r ".workflow_runs | map(select( .workflow_id == ${WORKFLOW_ID} )) | map(select( .head_sha == \"${HEAD_SHA}\" )) | sort_by( .run_number ) | .[-1] | .status" $RUNS_OUTPUT_JSON_FILE)
 
 	echo "$(date): check suite state: ${STATUS}"
 
@@ -63,7 +74,7 @@ git push $REMOTE --delete $BRANCH_NAME
 # git branch --delete $BRANCH_NAME
 
 
-CONCLUSION=$(jq -r ".workflow_runs[] | select( .head_sha == \"${HEAD_SHA}\" ) | select( .workflow_id == ${WORKFLOW_ID} ) | .conclusion" $RUNS_OUTPUT_JSON_FILE)
+CONCLUSION=$(jq -r ".workflow_runs | map(select( .workflow_id == ${WORKFLOW_ID} )) | map(select( .head_sha == \"${HEAD_SHA}\" )) | sort_by( .run_number ) | .[-1] |  .conclusion" $RUNS_OUTPUT_JSON_FILE)
 
 echo "conclusion: ${CONCLUSION}"
 
